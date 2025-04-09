8419637fa9a95319d2b5d48108c3d5b8bd517949
LunarKrystal <khoi0603k@gmail.com>
2025-04-09 12:35:26 +0700

    first commit

---
+++ modules/commands/prefix.js
@@ -0,0 +1,41 @@
+const axios = require("axios");
+const moment = require("moment-timezone");
+
+module.exports.config = {
+  name: "prefix",
+  version: "2.0.0",
+  hasPermission: 0,
+  credits: "DongDev",
+  description: "prefix bot",
+  commandCategory: "Hệ thống",
+  usages: "[]",
+  cooldowns: 0
+};
+
+module.exports.handleEvent = async function ({ api, event, client }) {
+  const { threadID, body } = event;
+  if (!body) return;
+
+  const { PREFIX } = global.config;
+  const gio = moment.tz("Asia/Ho_Chi_Minh").format("HH:mm:ss || DD/MM/YYYY");
+
+  let threadSetting = global.data.threadData.get(threadID) || {};
+  let prefix = threadSetting.PREFIX || PREFIX;
+
+  const lowerBody = body.toLowerCase();
+
+  if (
+    lowerBody === "prefix" ||
+    lowerBody === "prefix bot là gì" ||
+    lowerBody === "quên prefix r" ||
+    lowerBody === "dùng sao"
+  ) {
+    api.sendMessage(
+      `✏️ Prefix của nhóm: ${prefix}\n📎 Prefix hệ thống: ${PREFIX}`,
+      threadID,
+      event.messageID
+    );
+  }
+};
+
+module.exports.run = async function () {};
